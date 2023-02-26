0515a9865899385e55ad91db6acd29e390195803
Huang Jia Yi <3a932117@gm.student.ncut.edu.tw>
2023-02-27 02:33:03 +0800

    匯入模板

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>@yield('title')</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="{{asset('assets/favicon.ico')}}" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="{{asset('css/styles.css')}}" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+        @include('layouts.partials.navigation')
+        <!-- Header-->
+        @yield('content');
+        <!-- Footer-->
+        @include('layouts.partials.footer');
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="{{asset('js/scripts.js')}}"></script>
+        <script src="{{asset('js/tw-city-selector.js')}}"></script>
+        <script>
+            new TwCitySelector();
+        </script>
+    </body>
+</html>
