731db7d2b215a84451bce629a95f44029bd1c9f7
Huang Jia Yi <3a932117@gm.student.ncut.edu.tw>
2023-03-02 23:06:46 +0800

    房東管理頁面 模板匯入

---
+++ resources/views/layouts/owner_partials/sidenav.blade.php
@@ -0,0 +1,27 @@
+<div id="layoutSidenav_nav">
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+            <div class="nav">
+                <a class="nav-link" href="#">
+                    <div class="sb-nav-link-icon">
+                        <i class="fas fa-tachometer-alt"></i>
+                    </div>
+                    要打的
+                </a>
+                <a class="nav-link" href="#">
+                    <div class="sb-nav-link-icon">
+                        <i class="fas fa-tachometer-alt"></i>
+                    </div>
+                    要打的
+                </a>
+                <a class="nav-link" href="#">
+                    <div class="sb-nav-link-icon">
+                        <i class="fas fa-tachometer-alt"></i>
+                    </div>
+                    要打的
+                </a>
+            </div>
+        </div>
+
+    </nav>
+</div>
---
+++ resources/views/owners/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.owner_master')
+@section('title', '房東管理頁面')
+@section('page-content')
+    <div class="from-group mb-3 px-3 py-2">
+        <label for="content" class="form-label">新增出租地點</label>
+        <input type="text" class="input-group-sm">
+        <button type="button" class="btn btn-dark btn-sm">新增</button>
+    </div>
+@endsection
