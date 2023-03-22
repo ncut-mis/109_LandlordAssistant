7a5234c42052f810da7113a17c60eee898ee083a
xu qian su <s3a932003@student.ncut.edu.tw>
2023-03-22 13:14:02 +0800

    新增地點公告

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\House;
-use App\Models\Image;
 use App\Models\Location;
 use App\Models\Furnish;
 use App\Models\Expense;
@@ -27,11 +26,9 @@ class HouseController extends Controller
      */
     public function create($location)
     {
-		$locations = Location::find($location);
-		$owner_id = $locations->owner->id;
-		$locations_data = [
+        $locations = Location::find($location);
+        $locations_data = [
             'locations' => $locations,
-            'owner_id' => $owner_id,
         ];
         return view('owners.locations.houses.create',$locations_data);
     }
@@ -45,138 +42,49 @@ class HouseController extends Controller
      */
     public function store(Request $request,$location)
     {
-		//dd($request->image);
         $validatedData = $request->validate([
-			'name' => 'required|max:255',
-			'address' => 'required|max:255',
-			'furnish.*' => 'required|max:255',
-		], [
-			'name.required' => '請輸入房屋名稱。',
-			'name.max' => '房屋名稱不得超過255個字元。',
-			'address.required' => '請輸入地址。',
-			'address.max' => '地址不得超過255個字元。',
-			'furnish.*.required' => '請輸入設備。',
-			'furnish.*.max' => '設備不得超過255個字元。',
-		]);
-		
-		//狀態等房屋底下的尚未全部可儲存
-		
-		$l = Location::find($location);
-		$owner_id = $l->owner->id;
+            'name' => 'required|max:255',
+            'address' => 'required|max:255',
+            'furnish' => 'required|max:255',
+        ]);
 
-		if(isset($_REQUEST['publish'])){
-			$validatedData1 = $request->validate([
-				'name' => 'required',
-				'address' => 'required',
-				'introduce' => 'required',
-				'amount' => 'required',
-				'interval' => 'required',
-				'num_renter' => 'required',
-				'min_period' => 'required',
-				'pattern' => 'required',
-				'size' => 'required',
-				'type' => 'required',
-				'floor' => 'required',
-				'furnishings' => 'required',
-				'features' => 'required',
-			],[
-				'name.required' => '請輸入房屋名稱。',
-				'address.required' => '請輸入地址。',
-				'introduce.required' => '請輸入介紹。',
-				'amount.required' => '請輸入金額。',
-				'interval.required' => '請輸入繳納區間。',
-				'num_renter.required' => '請輸入可住人數。',
-				'min_period.required' => '請輸入最短租期。',
-				'pattern.required' => '請輸入格局。',
-				'size.required' => '請輸入坪數。',
-				'type.required' => '請輸入類型。',
-				'floor.required' => '請輸入樓層。',
-				'furnishings.required' => '請輸入設備。',
-				'features.required' => '請輸入特色。',
-			]);
-			$lease_status = "已刊登";
-		}else if(isset($_REQUEST['unpublish'])){
-			$lease_status = "閒置";
-		}
+        //狀態等房屋底下的尚未全部可儲存
 
-		// 建立 House 資料
-		$house = House::create([
+        $l = Location::find($location);
+        $owner_id = $l->owner->id;
+        // 建立 House 資料
+        $house = House::create([
             'name' => $validatedData['name'],
             'address' => $validatedData['address'],
-			'lease_status' => $lease_status,
-			'introduce' => $request->introduce,
-			'num_renter' => $request->num_renter,
-			'min_period' => $request->min_period,
-			'pattern' => $request->pattern,
-			'size' => $request->size,
-			'type' => $request->type,
-			'floor' => $request->floor,
-			'location_id' => $location,
-			'owner_id' => $owner_id,
+            'introduce' => $request->introduce,
+            'location_id' => $location,
+            'owner_id' => $owner_id,
         ]);
+        $house_id = $house->id;
+        // 建立 Furnish 資料
+        $furnish = new Furnish([
+            'house_id' => $house_id,
+            'furnish' => $validatedData['furnish'],
+        ]);
+        // 透過關聯存取資料庫
+        $house->furnishings()->save($furnish);
 
-		// 儲存動態增加的圖片路徑
-        //$images = $request->input('image', []);
-		//        foreach($request->file('image') as $image){
-
-		//foreach($request->image as $key => $image) {
-		if($request->file('image') != null){
-			foreach($request->file('image') as $key => $image) {
-				//影像圖檔名稱
-				$file_name = $request->id.'_'.time().'.'.$image->getClientOriginalExtension();
-				//把檔案存到公開的資料夾
-				$file_path = public_path('image/'.$file_name);
-				move_uploaded_file($image->getPathname(), $file_path);
-
-				//$file_path = $image->move(public_path('image'), $image);
-				//$path = $image->storeAs('public/images', $image->getClientOriginalName());
-				//$relativePath = str_replace('public', '/storage', $path);
-				$imageModel = new Image([
-					'house_id' => $house->id,
-					'image' => $file_name,
-				]);
-				//dd($imageModel);
-				$house->image()->save($imageModel);
-			}
-		}
-		$house_id = $house->id;
-
-		// 建立 Expense 資料
+        // 建立 Expense 資料
         $expense = new Expense([
             'house_id' => $house_id,
-            'type' => "租金",
             'amount' => $request->amount,
-            'interval' => $request->interval,
         ]);
-		// 透過關聯存取資料庫
+        // 透過關聯存取資料庫
         $house->expenses()->save($expense);
 
-
-		// 建立 Furnish 資料
-		if ($request->furnishings !== null) {
-			// 新增特色
-			foreach ($request->furnishings as $furnish) {
-				$newFurnish = new Furnish([
-					'house_id' => $house->id,
-					'furnish' => $furnish,
-				]);
-				$house->furnishings()->save($newFurnish);
-			}
-		}
-
-		// 建立 Feature 資料
-		if ($request->features !== null) {
-			// 新增特色
-			foreach ($request->features as $feature) {
-				$newFeature = new Feature([
-					'house_id' => $house->id,
-					'feature' => $feature,
-				]);
-				$house->features()->save($newFeature);
-			}
-		}
-
-		return redirect()->route('owners.home.index',$owner_id)->with('success', '儲存成功！');
+        // 建立 Feature 資料
+        $feature = new Feature([
+            'house_id' => $house_id,
+            'feature' => $request->feature,
+        ]);
+        // 透過關聯存取資料庫
+        $house->features()->save($feature);
+        return redirect()->route('owners.houses.index')->with('success', '儲存成功！');
     }
 
     /**
@@ -201,24 +109,16 @@ class HouseController extends Controller
      */
     public function edit(Location $location, House $house)
     {
-		$furnish = $house->furnishings;
-		$amount = $house->expenses;
-		$feature = $house->features;
-		$locations_data = [
+        $furnish = $house->furnishings;
+        $amount = $house->expenses;
+        $feature = $house->features;
+        $locations_data = [
             'locations' => $location,
             'houses' => $house,
             'amount' => $amount,
             'furnish' => $furnish,
             'feature' => $feature,
-<<<<<<< HEAD
-			
-=======
 
-
-
-            'owner_id' => $location->owner_id,
-
->>>>>>> origin/master
         ];
         return view('owners.locations.houses.edit',$locations_data);
     }
@@ -232,168 +132,40 @@ class HouseController extends Controller
      */
     public function update(Request $request, Location $location, House $house)
     {
-<<<<<<< HEAD
-		//初步版本，尚未全部可修改
-		
-		// 從請求中獲取表單提交的數據
-=======
+        //初步版本，尚未全部可修改
 
->>>>>>> origin/master
-		$data = $request->only([
-			'name',
-			'address',
-			'introduce',
-			'num_renter',
-			'min_period',
-			'pattern',
-			'size',
-			'type',
-			'floor',
-		]);
+        // 從請求中獲取表單提交的數據
+        $data = $request->only([
+            'name',
+            'address',
+            'introduce'
+        ]);
+        $house->update($data);
 
-		if(isset($_REQUEST['publish']) || isset($_REQUEST['unpublish'])){
-			if(isset($_REQUEST['publish'])){
-				$lease_status = "已刊登";
-			}else if(isset($_REQUEST['unpublish'])){
-				$lease_status = "閒置";
-			}
-			// 從請求中獲取表單提交的數據
-			$data = array_merge(
-				['lease_status' => $lease_status],$data
-			);
-		}
-		//dd($data);
-		$house->update($data);
-<<<<<<< HEAD
-		
-		// 更新房屋信息
-=======
+        // 更新房屋信息
+        if ($house->expenses !== null) {
+            foreach ($house->expenses as $expense) {
+                $expense->amount = $request->amount;
+                $expense->save();
+            }
+        }
 
-		// 檢查照片是否需要刪除
-		if ($request->images !== null) {
-			//獲取所有照片
-			foreach ($request->images as $image) {
-						//dump($image);
-				$existingImage = $house->image()->pluck('image')->all();
-						//dump($existingImage);
-					//找出那些照片不在 $request->images 集合中的照片
-				$imagesToDelete = array_diff($existingImage, $request->images);
-						//dump($imagesToDelete);
-				if($imagesToDelete !== null){
-					foreach ($imagesToDelete as $imageToDelete) {
-						$house->image()->where('image', $imageToDelete)->delete();
-					}
-				}else{
-					$house->image()->where('image', $imageToDelete)->delete();
-				}
-			}
-		}else{
-			$house->image()->delete();
-		}
-		//dd($request->file('image'));
+        if ($house->furnishings !== null) {
+            foreach ($house->furnishings as $furnishing) {
+                $furnishing->furnish = $request->furnish;
+                $furnishing->save();
+            }
+        }
 
-		//更新照片
-		if ($request->file('image') !== null) {
-			// 新增照片
-			foreach ($request->file('image') as $key => $image) {
-				// 不存在照片的設備，可以新增
-				//影像圖檔名稱
-				$file_name = $request->id.'_'.uniqid().'.'.$image->getClientOriginalExtension();
-				//把檔案存到公開的資料夾
-				$file_path = public_path('image/'.$file_name);
-				//dump($file_name);
-				move_uploaded_file($image->getPathname(), $file_path);
-				$newImage = new Image([
-					'house_id' => $house->id,
-					'image' => $file_name,
-				]);
+        if ($house->feature !== null) {
+            foreach ($house->feature as $feature) {
+                $feature->feature = $request->feature;
+                $feature->save();
+            }
+        }
 
-
-				$house->image()->save($newImage);
-			}
-		}
-
-		/*foreach($request->file('image') as $key => $image) {
-			//影像圖檔名稱
-			$file_name = $request->id.'_'.time().'.'.$image->getClientOriginalExtension();
-            //把檔案存到公開的資料夾
-			$file_path = public_path('image/'.$file_name);
-			move_uploaded_file($image->getPathname(), $file_path);
-
-			//$file_path = $image->move(public_path('image'), $image);
-			//$path = $image->storeAs('public/images', $image->getClientOriginalName());
-			//$relativePath = str_replace('public', '/storage', $path);
-			$imageModel = new Image([
-				'house_id' => $house->id,
-				'image' => $file_name,
-			]);
-			//dd($imageModel);
-            $house->image()->save($imageModel);
-        }*/
-
-		// 更新租屋費用信息
-
->>>>>>> origin/master
-		if ($house->expenses !== null) {
-			foreach ($house->expenses as $expense) {
-				$expense->amount = $request->amount;
-				$expense->interval = $request->interval;
-				$expense->save();
-			}
-		}
-		//更新設備
-		if ($request->furnishings !== null) {
-			// 新增設備
-			foreach ($request->furnishings as $furnish) {
-				// 檢查設備是否已經存在
-				if (!$house->furnishings()->where('furnish', $furnish)->exists()) {
-					// 不存在重複的設備，可以新增
-					$newFurnish = new Furnish([
-						'house_id' => $house->id,
-						'furnish' => $furnish,
-					]);
-					$house->furnishings()->save($newFurnish);
-				}
-			}
-			// 檢查設備是否需要刪除
-				//獲取所有設備
-			$existingFurnishings = $house->furnishings()->pluck('furnish')->all();
-				//找出那些設備不在 $request->furnishings 集合中的設備
-			$furnishingsToDelete = array_diff($existingFurnishings, $request->furnishings);
-			foreach ($furnishingsToDelete as $furnishToDelete) {
-				$house->furnishings()->where('furnish', $furnishToDelete)->delete();
-			}
-		}
-
-		//更新特色
-		if ($request->features !== null) {
-			// 新增特色
-			foreach ($request->features as $feature) {
-				// 檢查特色是否已經存在
-				if (!$house->features()->where('feature', $feature)->exists()) {
-					// 不存在重複的特色，可以新增
-					$newFeature = new Feature([
-						'house_id' => $house->id,
-						'feature' => $feature,
-					]);
-					$house->features()->save($newFeature);
-				}
-			}
-
-			// 檢查特色是否需要刪除
-				//獲取所有特色
-			$existingFeatures = $house->features()->pluck('feature')->all();
-			//dump($existingFeatures);
-				//找出那些特色不在 $request->features 集合中的特色
-			$featuresToDelete = array_diff($existingFeatures, $request->features);
-			//dd($featuresToDelete);
-			foreach ($featuresToDelete as $featureToDelete) {
-				$house->features()->where('feature', $featureToDelete)->delete();
-			}
-		}
-
-		// 重定向到房屋管理頁面
-		return redirect()->route('owners.home.index', [$location->id, $house->id])->with('success', '修改成功！');
+        // 重定向到房屋管理頁面
+        return redirect()->route('owners.houses.index', [$location->id, $house->id])->with('success', '修改成功！');
     }
 
     public function publish_update()
@@ -411,14 +183,9 @@ class HouseController extends Controller
     public function destroy(Location $location, House $house)
     {
         if (!$house) {
-			return redirect()->back()->with('error', '房屋找不到');
-		}
-		if($house->lease_status !== "出租中"){
-			$house->delete();
-			return redirect()->back()->with('success', '房屋刪除成功');
-		}else{
-			return redirect()->back()->with('error', '房屋刪除失敗');
-		}
-
+            return redirect()->back()->with('error', '房屋找不到');
+        }
+        $house->delete();
+        return redirect()->back()->with('success', '房屋刪除成功');
     }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Location;
 use App\Models\Post;
 use App\Http\Requests\StorePostRequest;
+use Illuminate\Http\Request;
 use App\Http\Requests\UpdatePostRequest;
 
 class PostController extends Controller
@@ -16,27 +18,54 @@ class PostController extends Controller
 
     }
 
-    public function owners_index()
+    public function owners_index($location_id)
     {
-        $posts = Post::orderBy('created_at', 'DESC')->get();
 
-        $data = ['posts' => $posts];
-        return view('owners.locations.posts.index', $data);
+        $location = Location::with(['posts' => function ($query) {
+            $query->orderBy('created_at', 'desc'); // 按照创建时间降序排列帖子
+        }])->findOrFail($location_id);
+        return view('owners.locations.posts.index', [
+            'locations' => collect([$location]),
+            'location' => $location
+        ]);
     }
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-       // return view('owners.locations.posts.create');
+//       dd($location) ;
+        $location = Location::with('posts')->get();
+        $location_data=['location'=>$location];
+        return view('owners.locations.posts.create', $location_data );
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request)
+    public function store(Location $location, Request $request )
     {
-        //
+        $this->validate($request, [
+            'title' => 'required|min:2|max:50',
+            'content' => 'required',
+
+        ]);
+        $post = new Post([
+            'title' => $request->input('title'),
+            'content' => $request->input('content'),
+            'date' => $request->input('date'),
+        ]);
+//        真實用戶
+//        $post->user_id = auth()->user()->id;
+        $post->user_id = 1;
+        $location->posts()->save($post);
+
+//        $request->merge(['location_id' => $location->id]);
+//        Post::create($request->all());
+        //dump &die
+        //dd($request->all());
+
+        return redirect()->route('owners.locations.posts.index',$location);
     }
 
     /**
@@ -50,25 +79,17 @@ class PostController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Location $location, Post $post)
-
+    public function edit(Post $post)
     {
-        return view('owners.locations.posts.edit', compact('location', 'post'));
+        //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Location $location, Post $post,Request $request)
+    public function update(UpdatePostRequest $request, Post $post)
     {
-        $this->validate($request, [
-            'title' => 'required|min:2|max:50',
-            'content' => 'required',
-        ]);
-
-        $post->update($request->all());
-
-        return redirect()->route('owners.locations.posts.index', $location);
+        //
     }
 
     /**
