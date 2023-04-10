57ec3901a121839dfbbb771f9a2a091a8a354b3c
Huang Jia Yi <3a932117@gm.student.ncut.edu.tw>
2023-04-10 22:39:36 +0800

    租屋資訊網新模版-測試

---
+++ resources/views/layouts/test_master.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<!--
+	TXT by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+    <title>@yield('title')</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="css/main.css" />
+</head>
+<body class="homepage is-preload">
+<div id="page-wrapper">
+
+    <!-- Nav -->
+    @include('layouts.test_partials.navigation')
+
+    <!-- Main -->
+    @yield('content');
+
+    <!-- Footer -->
+    @include('layouts.test_partials.footer');
+
+</div>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.dropotron.min.js"></script>
+<script src="assets/js/jquery.scrolly.min.js"></script>
+<script src="assets/js/browser.min.js"></script>
+<script src="assets/js/breakpoints.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
