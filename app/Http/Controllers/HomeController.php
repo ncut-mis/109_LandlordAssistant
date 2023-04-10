97dd9c022249aeaf984ccfe07aa3993b22960299
Huang Jia Yi <3a932117@gm.student.ncut.edu.tw>
2023-04-11 03:41:02 +0800

    測試用web

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
         $view_data = [
             'houses' => $houses,
         ];
-        return view('index', $view_data);
+        return view('test_index', $view_data);
     }
 
 
@@ -58,17 +58,17 @@ class HomeController extends Controller
             'image' => $image,
             'expenses' => $expenses,
 		];
-		return view('houses.show',$data);
+		return view('houses.test_show',$data);
     }
 
     public function about()
     {
-        return view('about');
+        return view('test_about');
     }
 
     public function help()
     {
-        return view('help');
+        return view('test_help');
     }
     /**
      * Show the form for editing the specified resource.
