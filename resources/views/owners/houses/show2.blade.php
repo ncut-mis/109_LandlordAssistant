54fa56b1c6840b396bc0975c1208c64d0721cbec
xu qian su <s3a932003@student.ncut.edu.tw>
2023-05-10 17:19:12 +0800

    房東地點公告版面更改

--- resources/views/owners/houses/show2.blade.php
+++ resources/views/owners/houses/show2.blade.php
@@ -34,12 +34,6 @@
                                         </li>
                                         <li class="nav-item">
                                             <button type="button" class="nav-link" role="tab" data-bs-toggle="tab"
-                                                    data-bs-target="#navs-top-post" aria-controls="navs-top-profile"
-                                                    aria-selected="false">公告
-                                            </button>
-                                        </li>
-                                        <li class="nav-item">
-                                            <button type="button" class="nav-link" role="tab" data-bs-toggle="tab"
                                                     data-bs-target="#navs-top-expense" aria-controls="navs-top-messages"
                                                     aria-selected="false">費用
                                             </button>
@@ -326,21 +320,6 @@
                                                     <!--/ Transactions -->
                                                 </div>
                                             </div>
-                                        </div>
-                                        <!--公告資訊內容-->
-                                        <div class="tab-pane fade" id="navs-top-post" role="tabpanel">
-                                            <p>
-                                                Donut dragée jelly pie halvah. Danish gingerbread bonbon cookie wafer
-                                                candy oat cake ice cream. Gummies
-                                                halvah
-                                                tootsie roll muffin biscuit icing dessert gingerbread. Pastry ice cream
-                                                cheesecake fruitcake.
-                                            </p>
-                                            <p class="mb-0">
-                                                Jelly-o jelly beans icing pastry cake cake lemon drops. Muffin muffin
-                                                pie tiramisu halvah cotton candy
-                                                liquorice caramels.
-                                            </p>
                                         </div>
                                         <!--費用資訊內容-->
 
