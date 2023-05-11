87ef5df9245b0bcf8a35eef0ad3329a3b9f82d1f
3A932051 <asdf.tw12345@gmail.com>
2023-05-12 02:17:38 +0800

    修正很多合併後產生的錯誤 還有登入後讓註冊登入按鈕消失  未登入不顯示登出

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -7,18 +7,18 @@
                 <li><a href="{{url('users/1')}}" style="text-align: center">個人資料</a></li>
                 <li><a href="{{url('users/owners/1')}}" style="text-align: center">切換 房東 身分</a></li>
                 <li><a href="{{url('renters/houses')}}" style="text-align: center">切換 租客 身分</a></li>
-                <li><a href="#" style="text-align: center">登出</a></li>
             </ul>
         <li><a href="{{url('about')}}">關於我們</a></li>
         <li><a href="{{url('help')}}">幫助</a></li>
+        @if (!Auth::check())
         <li><a href="{{route('register')}}">註冊</a></li>
         <li> <a href="{{route('login')}}">登入</a></li>
-
-        <form method="POST" action="{{ route('logout') }}" style="display: inline-block;">
-            @csrf
-            @method('DELETE')
-            <li> <a href="{{route('logout')}}" type="submit">登出</a></li>
-        </form>
-
+        @endif
+        @if (Auth::check())
+            <li> <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">登出</a></li>
+            <form id="logout-form" method="POST" action="{{ route('logout') }}" style="display: none;">
+                @csrf
+            </form>
+       @endif
     </ul>
 </nav>
