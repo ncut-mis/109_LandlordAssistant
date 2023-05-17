9100f61ac4acb74a83814c4b619f046700f9615e
xu qian su <s3a932003@student.ncut.edu.tw>
2023-05-17 17:53:00 +0800

    修改租屋資訊首頁的navigation中的連結

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -4,12 +4,12 @@
         <li>
             <a href="#">會員中心</a>
             <ul>
-                <li><a href="{{url('users/1')}}" style="text-align: center">個人資料</a></li>
-                <li><a href="{{url('users/owners/1')}}" style="text-align: center">切換 房東 身分</a></li>
-                <li><a href="{{url('renters/houses')}}" style="text-align: center">切換 租客 身分</a></li>
+                <li><a href="{{url('users/{user}')}}" style="text-align: center">個人資料</a></li>
+                <li><a href="{{url('users/owners/{owner}')}}" style="text-align: center">切換 房東 身分</a></li>
+                <li><a href="{{url('users/renters/{renter}')}}" style="text-align: center">切換 租客 身分</a></li>
             </ul>
-        <li><a href="{{url('about')}}">關於我們</a></li>
-        <li><a href="{{url('help')}}">幫助</a></li>
+{{--        <li><a href="{{url('about')}}">關於我們</a></li>--}}
+{{--        <li><a href="{{url('help')}}">幫助</a></li>--}}
         @if (!Auth::check())
         <li><a href="{{route('register')}}">註冊</a></li>
         <li> <a href="{{route('login')}}">登入</a></li>
