957a1d879047fed957146b3cc1ff2ea32e3d7c29
3A932004 <s3a932004@student.ncut.edu.tw>
2023-05-19 23:36:57 +0800

    互動視窗

--- resources/views/renters/houses/index.blade.php
+++ resources/views/renters/houses/index.blade.php
@@ -16,6 +16,35 @@
             {{ session('no') }}
         </div>
     @endif
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/js/bootstrap.bundle.min.js"></script>
+	@if($posts->isNotEmpty())
+		<script>
+			$(document).ready(function() {
+				$('#post').modal('show');
+			});
+		</script>
+	@endif
+
+	<button type="button" class="col-sm-3 btn btn-primary" data-bs-toggle="modal" data-bs-target="#post">確定送出</button>
+
+	<div class="modal" tabindex="-1" id="post">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title">Modal title</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<p>Modal body text goes here.</p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+			</div>
+		</div>
+	</div>
     <div class="layout-wrapper">
         <div class="layout-container">
             <!-- Menu -->
